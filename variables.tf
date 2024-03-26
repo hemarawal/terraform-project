d7b6b9cecfbdd4ab753712ddf93cf56ad1407adc
hemarawal <56906330+hemarawal@users.noreply.github.com>
2024-03-26 12:17:02 +0530

    Create variables.tf
    added variables.tf file

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "key_pair_public_key_path" {}
+variable "private_key_path" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "ssh_user" {}
+variable "vpc_cidr_block" {}
+variable "ebs_volume_size" {}
+variable "ebs_device_name" {}
